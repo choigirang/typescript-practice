2f92b93671ad813e3786df953d8cfc9574cf2d3e
GiRang Choi <choigirang@kakao.com>
2023-03-26 10:49:24 +0900

    feat: 클래스(제네릭)
    함수와 같이 클래스에서도 제네릭을 사용할 수 있다.
    public 개념

--- src/15.제네릭_클래스.ts
+++ src/15.제네릭_클래스.ts
@@ -17,4 +17,30 @@ class User1<P> {
   }
 }
 
-const ts15User = new User1({});
+interface UserAType {
+  name: string;
+  age: number;
+  isValid: boolean;
+}
+
+interface UserBType {
+  name: string;
+  age: number;
+  email: string[];
+}
+
+const ts15User = new User1<UserAType>({
+  name: "string",
+  age: 1,
+  isValid: true,
+  emails: [], // UserAType과 일치하지 않음
+});
+// 함수에서의 제네릭과 같이 사용가능
+
+const neo = new User1<UserBType>({
+  name: "string",
+  age: NaN,
+  email: ["asd"],
+});
+
+export {};
